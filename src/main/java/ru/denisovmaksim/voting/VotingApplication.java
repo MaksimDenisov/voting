1df5ef4009148ccd612a77339a4ec9c338b30b2a
max <max.nick.demax@gmail.com>
2024-01-07 14:51:14 +0300

    Initial commit

---
+++ src/main/java/ru/denisovmaksim/voting/VotingApplication.java
@@ -0,0 +1,13 @@
+package ru.denisovmaksim.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingApplication.class, args);
+    }
+
+}
